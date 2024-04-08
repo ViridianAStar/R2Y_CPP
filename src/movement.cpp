5cec5db51fee52611ed43176417f17b6dc13b4e0
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2024-04-08 09:01:38 -0400

    This has a chance of working

--- src/movement.cpp
+++ src/movement.cpp
@@ -101,48 +101,48 @@ using namespace vex;
         rightside.stop(hold);
       }
 
-      // go a distance while turning
-      void movement::move_distance_spiral(float distance, float desired_end_angle) {
+      void movement::movingSwingleft(float radius, float angle) {
+        // so we don't have to deal with differential jank
+        float initialpositionLeft = leftMotors.position(deg);
+        float initialpositionRight = rightMotors.position(deg);
 
-        // first we convert desired linear distance to degrees
-        float degreesWanted = ((distance*360)/(circumference*gearRatio));
-        printf("desired %f\n", degreesWanted);
-        // we record the initial average position of the motors
-        float initialavgPosition = ((leftside.position(deg) + rightside.position(deg))/2);
-        printf("init %f\n", initialavgPosition);
-        // your start average position is your initial average position
-        float avgPositon = initialavgPosition;
+        float distanceLeft = (((2*M_PI)*radius)*(angle/360));
+        float distanceRight = (((2*M_PI)*(radius+WheelBase))*(angle/360))
 
-        // record your initial heading
-        float heading = reduce_0_to_360(rotationalSensor.rotation());
-        // your desired heading is your initial heading this keeps you on a straight line
-        float desired_heading = heading;
+        float desiredLeft = ((distanceLeft*360)/(circumference*gearRatio));
+        float desiredRight = ((distanceRight*360)/(circumference*gearRatio));
 
-        // initialize PIDs
-        pid lateral = pid(lkP, lkI, lkD, laiwValue, Timeout, settleTime, lsettleBounds, lmv);
-        pid rotational = pid(rkP, rkI, rkD, raiwValue, Timeout, settleTime, rsettleBounds, (lmv*0.6));
+        float avgPositionleft = initialpositionLeft;
+
+        float avgPositionright = initialpositionRight;
+
+        pid left = pid(lkP*0.75, lkI*0.75, lkD*0.75, laiwValue, Timeout, settleTime, lsettleBounds, (lmv-1.334));
+        pid right = pid(lkP, lkI, lkD, laiwValue, Timeout, settleTime, lsettleBounds, lmv);
         
-        while ((lateral.active() == true && rotational.active() == true) || (lateral.active() == false && rotational.active() == true)) {
-          // while youre lateral pid is active calculate your average position and heading
-          avgPositon = ((leftside.position(deg) + rightside.position(deg))/2);
-          heading = reduce_0_to_360(rotationalSensor.rotation());
+        while (right.active() == true) {
+          avgPositionleft = leftMotors.position(deg);
+          avgPositionright = rightMotors.position(deg);
 
-          // calculate your errors to give to PID (we say that our desired position is our initial average positon + our desired distance)
-          float lateralerror = (degreesWanted + initialavgPosition) - avgPositon;
-          printf("lerror %f\n", lateralerror);
-          float headingerror = reduce_negative_180_to_180(desired_heading - heading);
+          float lefterror = (desiredLeft + initialpositionLeft) - avgPositionleft;
+          float righterror = (desiredRight + initialpositionRight) - avgPositionright;
 
-          // spin our motors with the desired amounts of power.
-          leftside.spin(forward, (lateral.calcPID(lateralerror) + rotational.calcPID(headingerror)), volt);
-          rightside.spin(forward, (lateral.calcPID(lateralerror) - rotational.calcPID(headingerror)), volt);
+          rightMotors.spin(forward, right.calcPID(righterror), volt);
+          if (left.active() == true) {
+            leftMotors.spin(forward, left.calcPID(lefterror), volt);
+          } else {
+            leftMotors.stop(hold);
+          }
 
-          // stop doing things for 10 milliseconds after every loop
           task::sleep(10);
         }
 
         // when its not active, stop.
         leftside.stop(hold);
         rightside.stop(hold);
+      }
+
+      void movement::movingSwingright(float radius, float angle) {
+        
       }
 
       // point front towards a given angle.
